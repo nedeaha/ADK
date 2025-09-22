b855b38588440c241b7f1bee4db3f1bb2b424120
nedea <nedea.haukebo@gmail.com>
2025-09-22 16:57:11 +0200

    skibidi jag har tänkt lite

--- lab2/ClosestWords.java
+++ lab2/ClosestWords.java
@@ -1,46 +1,32 @@
 /* Labb 2 i DD2350 Algoritmer, datastrukturer och komplexitet    */
 /* Se labbinstruktionerna i kursrummet i Canvas                  */
 /* Ursprunglig författare: Viggo Kann KTH viggo@nada.kth.se      */
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 public class ClosestWords {
   LinkedList<String> closestWords = null;
 
   int closestDistance = -1;
 
-  int partDist(String w1, String w2, int w1len, int w2len) {
-    int[] prevRow = new int[w2len + 1];
-    int[] currRow = new int[w2len + 1];
+  int[][] prevM = null;
 
-    for (int j = 0; j <= w2len; j++) {
-      prevRow[j] = j;
-    }
-
-    for (int i = 1; i <= w1len; i++) {
-      currRow[0] = i;
-      for (int j = 1; j <= w2len; j++) {
-        int x = ((w1.charAt(i - 1) == w2.charAt(j - 1)) ? 0 : 1);
-        currRow[j] = Math.min(Math.min(prevRow[j] + 1, // Deletion
-            currRow[j - 1] + 1), // Insertion
-            prevRow[j - 1] + x); // Substitution
-      }
-
-      // Swap rows
-      int[] temp = prevRow;
-      prevRow = currRow;
-      currRow = temp;
-    }
-
-    return prevRow[w2len];
-
-  }
-
-  int oldPartDist(String w1, String w2, int w1len, int w2len) {
+  int partDist(String w1, String w2, int w1len, int w2len, String p) {
     int[][] matrix = new int[w1len+1][w2len+1];
 
+    int n = 0;
+    if (p != null && w2 != null && prevM != null) {
+      while (n < w2.length() && n < p.length() && n < prevM.length && w2.charAt(n) == p.charAt(n)) {
+          if (prevM[n] == null) break;
+          matrix[n] = prevM[n];
+          n++;
+      }
+  }
+
     for (int i = 0; i <= w1len; i++) {
-      for (int j = 0; j <= w2len; j++) {
+      for (int j = n; j <= w2len; j++) {
         if (i == 0)
           matrix[i][j] = j;
         else if (j == 0)
@@ -53,40 +39,67 @@ public class ClosestWords {
         }
       } 
     }
+    prevM = matrix;
     return matrix[w1len][w2len];
   }
 
-  int originalPartDist(String w1, String w2, int w1len, int w2len) {
-    if (w1len == 0)
-      return w2len;
-    if (w2len == 0)
-      return w1len;
-    int res = partDist(w1, w2, w1len - 1, w2len - 1) + 
-	(w1.charAt(w1len - 1) == w2.charAt(w2len - 1) ? 0 : 1);
-    int addLetter = partDist(w1, w2, w1len - 1, w2len) + 1;
-    if (addLetter < res)
-      res = addLetter;
-    int deleteLetter = partDist(w1, w2, w1len, w2len - 1) + 1;
-    if (deleteLetter < res)
-      res = deleteLetter;
-    return res;
-  }
+  // int oldPartDist(String w1, String w2, int w1len, int w2len) {
+  //   int[][] matrix = new int[w1len+1][w2len+1];
 
-  int distance(String w1, String w2) {
-    return partDist(w1, w2, w1.length(), w2.length());
+  //   for (int i = 0; i <= w1len; i++) {
+  //     for (int j = 0; j <= w2len; j++) {
+  //       if (i == 0)
+  //         matrix[i][j] = j;
+  //       else if (j == 0)
+  //         matrix[i][j] = i;
+  //       else {
+  //         int x = ((w1.charAt(i - 1) == w2.charAt(j - 1)) ? 0 : 1);
+  //         matrix[i][j] = Math.min(Math.min(matrix[i - 1][j] + 1, // Deletion
+  //             matrix[i][j - 1] + 1), // Insertion
+  //             matrix[i - 1][j - 1] + x); // Substitution
+  //       }
+  //     } 
+  //   }
+  //   return matrix[w1len][w2len];
+  // }
+
+  // int originalPartDist(String w1, String w2, int w1len, int w2len) {
+  //   if (w1len == 0)
+  //     return w2len;
+  //   if (w2len == 0)
+  //     return w1len;
+  //   int res = partDist(w1, w2, w1len - 1, w2len - 1) + 
+	//   (w1.charAt(w1len - 1) == w2.charAt(w2len - 1) ? 0 : 1);
+  //   int addLetter = partDist(w1, w2, w1len - 1, w2len) + 1;
+  //   if (addLetter < res)
+  //     res = addLetter;
+  //   int deleteLetter = partDist(w1, w2, w1len, w2len - 1) + 1;
+  //   if (deleteLetter < res)
+  //     res = deleteLetter;
+  //   return res;
+  // }
+
+  int distance(String w1, String w2, String p) {
+    return partDist(w1, w2, w1.length(), w2.length(), p);
   }
 
   public ClosestWords(String w, List<String> wordList) {
+    String p = null;
+    prevM = null;
+
     for (String s : wordList) {
-      int dist = distance(w, s);
+      int dist = distance(w, s, p);
       // System.out.println("d(" + w + "," + s + ")=" + dist);
       if (dist < closestDistance || closestDistance == -1) {
         closestDistance = dist;
         closestWords = new LinkedList<String>();
         closestWords.add(s);
       }
-      else if (dist == closestDistance)
+      else if (dist == closestDistance) {
+
         closestWords.add(s);
+      } 
+    p = s;
     }
   }
 
