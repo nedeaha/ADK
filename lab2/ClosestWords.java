1fbe5c730747bc36d4849bf0cd697204abbfa035
nedea <nedea.haukebo@gmail.com>
2025-09-23 12:55:10 +0200

    sdfas

--- lab2/ClosestWords.java
+++ lab2/ClosestWords.java
@@ -1,10 +1,8 @@
 /* Labb 2 i DD2350 Algoritmer, datastrukturer och komplexitet    */
 /* Se labbinstruktionerna i kursrummet i Canvas                  */
 /* Ursprunglig författare: Viggo Kann KTH viggo@nada.kth.se      */
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Stack;
 
 public class ClosestWords {
   LinkedList<String> closestWords = null;
@@ -14,33 +12,40 @@ public class ClosestWords {
   int[][] prevM = null;
 
   int partDist(String w1, String w2, int w1len, int w2len, String p) {
-    int[][] matrix = new int[w1len+1][w2len+1];
+    int[][] matrix = new int[w2len+1][w1len+1];
 
-    int n = 0;
+    int n = 1;
     if (p != null && w2 != null && prevM != null) {
-      while (n < w2.length() && n < p.length() && n < prevM.length && w2.charAt(n) == p.charAt(n)) {
-          if (prevM[n] == null) break;
+      while (n < prevM.length && w2.charAt(n-1) == p.charAt(n-1)) {
           matrix[n] = prevM[n];
           n++;
       }
-  }
-
-    for (int i = 0; i <= w1len; i++) {
-      for (int j = n; j <= w2len; j++) {
-        if (i == 0)
-          matrix[i][j] = j;
-        else if (j == 0)
-          matrix[i][j] = i;
-        else {
-          int x = ((w1.charAt(i - 1) == w2.charAt(j - 1)) ? 0 : 1);
-          matrix[i][j] = Math.min(Math.min(matrix[i - 1][j] + 1, // Deletion
-              matrix[i][j - 1] + 1), // Insertion
-              matrix[i - 1][j - 1] + x); // Substitution
-        }
-      } 
     }
+    
+    for (int i = n; i <= w2len; i++) {
+        matrix[i][0] = i;
+    }
+
+    if (n == 1) {
+      for (int j = 0; j <= w1len; j++) {
+          matrix[0][j] = j;
+      }
+    }
+
+    for (int i = n; i <= w2len; i++) {
+      for (int j = 1; j <= w1len; j++) {
+        if (w2.charAt(i - 1) == w1.charAt(j - 1)) {
+          matrix[i][j] = matrix[i - 1][j - 1];
+        } else {
+          matrix[i][j] = Math.min(Math.min(matrix[i - 1][j], // Deletion
+              matrix[i][j - 1]), // Insertion
+              matrix[i - 1][j - 1]) + 1; // Substitution
+        }
+      }
+    } 
+
     prevM = matrix;
-    return matrix[w1len][w2len];
+    return matrix[w2len][w1len];
   }
 
   // int oldPartDist(String w1, String w2, int w1len, int w2len) {
